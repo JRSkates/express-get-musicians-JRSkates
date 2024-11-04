9a3c46d52378c664cc2810e79f6f74cb676d24b0
JRSkates <skatesproduction@gmail.com>
2024-11-04 13:02:37 +0000

    musicians endpoint test driven with sequelize query

--- index.test.js
+++ index.test.js
@@ -8,11 +8,19 @@ const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
 const {seedMusician} = require("./seedData");
+const Test = require('supertest/lib/test');
 
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
+    test("Musician endpoint", async () => {
+        const response = await request(app).get("/musicians")
+        expect(response.statusCode).toBe(200)
+        
+        const responseData = JSON.parse(response.text)
+        expect(responseData[0]).toHaveProperty("name")
+        expect(responseData[0].name).toBe("Mick Jagger")
+    })
     
 
 
